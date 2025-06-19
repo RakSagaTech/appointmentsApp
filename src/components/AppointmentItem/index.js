517e0fcf9dfe91886d0960a6cef99c69edaa36bb
rakesh <rakeshsagartfi21@gmail.com>
2025-06-19 13:03:34 +0000

    Added onChange, toggleIsStarred, onFilter, onAddAppointment

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -3,21 +3,31 @@
 import './index.css'
 
 const AppointmentItem = props => {
-  const {appointmentDetails} = props
+  const {appointmentDetails, toggleIsStarred} = props
   const {id, title, date, isStarred} = appointmentDetails
 
   const starImageUrl = isStarred
     ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
     : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  const onClickStar = () => {
+    toggleIsStarred(id)
+  }
+
   return (
     <li className="appointment-item">
       <div className="header-container">
         <h1 className="title"> {title} </h1>
-        <button type="button" className="star-button">
-          <img src={starImageUrl} alt="star" className="star"/>
+        <button
+          type="button"
+          data-testid="star"
+          className="star-button"
+          onClick={onClickStar}
+        >
+          <img src={starImageUrl} alt="star" className="star" />
         </button>
       </div>
-      <p className="date"> {date} </p>
+      <p className="date"> Date: {date} </p>
     </li>
   )
 }
